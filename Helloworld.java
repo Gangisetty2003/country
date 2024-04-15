fd59b0972ead8e318bcbdc773be829ed111b4320
Gangisetty2003 <ajaykumar.gangisetty3@gmail.com>
2024-04-15 18:30:54 +0530

    first commit

---
+++ Helloworld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+    public static void main(String[] args) {
+<<<<<<< HEAD
+        System.out.println("Hello, Ajay!"); 
+=======
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+>>>>>>> e2d0b6227535e5246e1881c71afcfc851559f112
+    }
+}
